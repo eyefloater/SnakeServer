2fe704ba4821e1344fd8e6a511a93b4d9ca7377f
eyefloater <gobifish@gmail.com>
2015-04-03 12:34:40 -0500

    first commit

---
+++ src/com/gaby/SnakeData.java
@@ -0,0 +1,26 @@
+package com.gaby;
+
+import java.awt.Point;
+
+public class SnakeData {
+
+	private String user;
+	private Point currentLoc;
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public Point getCurrentLoc() {
+		return currentLoc;
+	}
+
+	public void setCurrentLoc(Point currentLoc) {
+		this.currentLoc = currentLoc;
+	}
+
+}
---
+++ src/com/gaby/SnakeGame.java
@@ -0,0 +1,24 @@
+package com.gaby;
+
+public class SnakeGame {
+
+	private String first;
+	private String second;
+
+	public String getFirst() {
+		return first;
+	}
+
+	public void setFirst(String first) {
+		this.first = first;
+	}
+
+	public String getSecond() {
+		return second;
+	}
+
+	public void setSecond(String second) {
+		this.second = second;
+	}
+
+}
